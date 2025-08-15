ebdcca6a0b24b1012ed92453a5862339b4209c5d
Alex3am <110632806+Urdnaxeela@users.noreply.github.com>
2025-08-15 12:23:33 +0300

    Initial Next.js project with #111118 background screen

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main style={{
+      backgroundColor: '#111118',
+      height: '100vh',
+      width: '100vw',
+      margin: 0,
+      padding: 0
+    }}>
+    </main>
+  )
+}
